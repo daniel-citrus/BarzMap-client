9a52850ab171ef119c9c7881c2f6676b70a1b60a
Daniel <dandcalvo@gmail.com>
2026-03-26 15:27:34 -0700

    chore: migrate configs to ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), eslint({
+        cache: false,
+        include: ['src/**/*.{ts,tsx,js,jsx}'],
+    })],
 });
